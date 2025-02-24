232451d8a308f59b1a39ed4b0ba70b35d09e8ac6
Freycikkk <104511692+freycikkk@users.noreply.github.com>
2025-02-24 21:27:26 +0530

    Update shard.ts

--- src/commands/shard.ts
+++ src/commands/shard.ts
@@ -2,74 +2,86 @@ import { Client as DiscordClient, ButtonBuilder, ButtonStyle, Message } from 'di
 import type { Client } from '../'
 import { ProcessManager, inspect } from '../utils'
 
-export async function shard (message: Message, parent: Client): Promise<void> {
+export async function shard(message: Message, parent: Client): Promise<void> {
   if (!message.data.args) {
-    message.reply('Missing Arguments.')
-    return
+    message.reply("Missing Arguments.");
+    return;
   }
-  if (!parent.client.shard) {
-    message.reply('Shard Manager not found.')
-    return
+
+  if (!parent.client.shard && !parent.client.cluster) {
+    message.reply("Shard or Cluster Manager not found.");
+    return;
   }
-  let evalFunction: (client: DiscordClient) => unknown
+
+  let evalFunction: (client: DiscordClient) => unknown;
   try {
-    // eslint-disable-next-line no-new-func
-    evalFunction = Function('client', `return ${message.data.args}`) as (
+    evalFunction = Function("client", `return ${message.data.args}`) as (
       client: DiscordClient
-    ) => unknown // catch syntax error
+    ) => unknown;
   } catch (err) {
-    message.reply(err?.toString() ?? 'Error Occurred.')
-    return
+    message.reply(err?.toString() ?? "Error Occurred.");
+    return;
   }
-  const result = await parent.client.shard
-    .broadcastEval(evalFunction)
-    .then(el => el)
-    .catch(e => e.toString())
-  let msg
-  if (!Array.isArray(result)) { msg = new ProcessManager(message, result, parent, { lang: 'js' }) } else {
-    let sum
-    if (typeof result[0] === 'number') { sum = result.reduce((prev, val) => prev + val, 0) } else if (Array.isArray(result[0])) { sum = result.reduce((prev, val) => prev.concat(val), []) }
+
+  const isHybrid = Boolean(parent.client.cluster); // Check if hybrid sharding is used
+  const evalMethod = isHybrid
+    ? parent.client.cluster?.broadcastEval(evalFunction)
+    : parent.client.shard!.broadcastEval(evalFunction);
+
+  const result = await evalMethod?.catch((e) => e.toString());
+
+  let msg;
+  if (!Array.isArray(result)) {
+    msg = new ProcessManager(message, result, parent, { lang: "js" });
+  } else {
+    let sum;
+    if (typeof result[0] === "number") {
+      sum = result.reduce((prev, val) => prev + val, 0);
+    } else if (Array.isArray(result[0])) {
+      sum = result.reduce((prev, val) => prev.concat(val), []); // Flatten arrays
+    }
     msg = new ProcessManager(
       message,
       `// TOTAL\n${inspect(sum, { depth: 1, maxArrayLength: 50 })}\n\n${result
         .map(
           (value, index) =>
-            `// #${index} SHARD\n${inspect(value, {
+            `// #${index} ${isHybrid ? "CLUSTER" : "SHARD"}\n${inspect(value, {
               depth: 1,
-              maxArrayLength: 100
+              maxArrayLength: 100,
             })}`
         )
-        .join('\n')}`,
+        .join("\n")}`,
       parent,
-      { lang: 'js' }
-    )
+      { lang: "js" }
+    );
   }
 
-  await msg.init()
+  await msg.init();
   await msg.addAction([
     {
       button: new ButtonBuilder()
         .setStyle(ButtonStyle.Primary)
-        .setCustomId('dokdo$prev')
-        .setLabel('Prev'),
+        .setCustomId("dokdo$prev")
+        .setLabel("Prev"),
       action: ({ manager }) => manager.previousPage(),
-      requirePage: true
+      requirePage: true,
     },
     {
       button: new ButtonBuilder()
         .setStyle(ButtonStyle.Secondary)
-        .setCustomId('dokdo$stop')
-        .setLabel('Stop'),
+        .setCustomId("dokdo$stop")
+        .setLabel("Stop"),
       action: ({ manager }) => manager.destroy(),
-      requirePage: true
+      requirePage: true,
     },
     {
       button: new ButtonBuilder()
         .setStyle(ButtonStyle.Success)
-        .setCustomId('dokdo$next')
-        .setLabel('Next'),
+        .setCustomId("dokdo$next")
+        .setLabel("Next"),
       action: ({ manager }) => manager.nextPage(),
-      requirePage: true
-    }
-  ])
+      requirePage: true,
+    },
+  ]);
 }
+
